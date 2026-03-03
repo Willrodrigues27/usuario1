b6dbe60f65db6e5186736d2e530bfdb667938959
Will Rodrigues <162224706+Willrodrigues27@users.noreply.github.com>
2026-03-03 10:12:34 -0300

    inclusão de metodo put de usuário

--- src/main/java/com/javanauta/usuario1/business/UsuarioService.java
+++ src/main/java/com/javanauta/usuario1/business/UsuarioService.java
@@ -6,6 +6,7 @@ import com.javanauta.usuario1.infrastructure.entity.Usuario;
 import com.javanauta.usuario1.infrastructure.exceptions.ConflictException;
 import com.javanauta.usuario1.infrastructure.exceptions.ResourceNotFoundException;
 import com.javanauta.usuario1.infrastructure.repository.UsuarioRepository;
+import com.javanauta.usuario1.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -18,6 +19,7 @@ public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
     private final UsuarioConverter usuarioConverter;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
     public UsuarioDTO salvaUsuario(UsuarioDTO usuarioDTO){
         emailExiste(usuarioDTO.getEmail());
@@ -54,4 +56,21 @@ public class UsuarioService {
         usuarioRepository.deleteByEmail(email);
     }
 
+    public UsuarioDTO atualizaDadosUsuario(String token, UsuarioDTO dto){
+        // Aqui buscamos o email do usuário através do token (tirar a obrigatoreidade do email).
+        String email = jwtUtil.extrairEmailToken(token.substring(7));
+
+        //Criptografia de senha
+        dto.setSenha(dto.getSenha() != null ? passwordEncoder.encode(dto.getEmail()) : null);
+
+        //Busca os dados do usuário no banco de dados.
+        Usuario usuarioEntity = usuarioRepository.findByEmail(email).orElseThrow(() ->
+                new ResourceNotFoundException("Email não localizado"));
+
+        //Mesclou os dados que recebemos na requisição DTO com os dados do banco de dados
+        Usuario usuario = usuarioConverter.updateUsuario(dto, usuarioEntity);
+
+        //Salvou os dados do usuário e depois pegou o retorno e converteu para UsuarioDTO
+        return usuarioConverter.paraUsuarioDTO(usuarioRepository.save(usuario));
+    }
 }
--- src/main/java/com/javanauta/usuario1/business/converter/UsuarioConverter.java
+++ src/main/java/com/javanauta/usuario1/business/converter/UsuarioConverter.java
@@ -100,4 +100,15 @@ public class UsuarioConverter {
                 .ddd(telefoneDTO.getDdd())
                 .build();
     }
+
+    public Usuario updateUsuario(UsuarioDTO usuarioDTO, Usuario entity){
+        return Usuario.builder()
+                .nome(usuarioDTO.getNome() != null ? usuarioDTO.getNome() : entity.getNome())
+                .id(entity.getId())
+                .senha(usuarioDTO.getSenha() != null ? usuarioDTO.getSenha() : entity.getNome())
+                .email(usuarioDTO.getEmail() != null ? usuarioDTO.getEmail() : entity.getEmail())
+                .enderecos(entity.getEnderecos())
+                .telefones(entity.getTelefones())
+                .build();
+    }
 }
